cabd304de53f880678104e2e26c1c4222b5d2920
Odannys De La Cruz <el_odanis321@hotmail.com>
2020-12-20 17:09:35 -0500

    Configuracion de Particles.js para pagina index

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
 window.onload = function() {
+  Particles.init({
+        selector: '.particulas_fondo',
+        color: '#ffffff',
+        connectParticles: true,
+        maxParticles: 50,
+        speed: 1,
+        minDistance: 80,
+    });
+    Particles.init({
+        selector: '.particulas_frente',
+        color: '#ffffff',
+        connectParticles: true,
+        maxParticles: 30,
+        // speed: 2,
+        minDistance: 50,
+    })
     let links = document.querySelectorAll('.sin-permiso')
     links.forEach(link => {
         link.addEventListener('click', (e) => {
